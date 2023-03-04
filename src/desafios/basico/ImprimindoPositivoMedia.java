fe0e29b9ce43de7c4edb39f332ad440eaff67141
Daniel Neves <danielnevesdecastro@gmail.com>
2023-03-03 23:51:13 -0300

    Desafio de código Java Baśico

---
+++ src/desafios/basico/ImprimindoPositivoMedia.java
@@ -0,0 +1,30 @@
+package desafios.basico;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ImprimindoPositivoMedia {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in).useLocale(Locale.getDefault());
+        int cont = 0;
+        double media = 0;
+        double x;
+
+        //TODO: Implemente as condições adequadas para obter a quantidade
+        //de valores positivos e a média dos valores positivos:
+        double total = 0;
+        for (int i = 0; i <= 5; i++) {
+            x = leitor.nextDouble();
+            if (x > 0) {
+                cont++;
+                total += x;
+            }
+        }
+
+        leitor.close();
+        media = total / cont;
+        System.out.println(cont + " valores positivos");
+        System.out.println(String.format("%.1f", media));
+
+    }
+}
