ecb9edb46251b0cb381b8ae4819c47f6d580e0a4
Daniel Neves <danielnevesdecastro@gmail.com>
2023-03-06 12:11:40 -0300

    Desafio Fibonacci Facil

---
+++ src/desafios/basico/Dragao.java
@@ -0,0 +1,24 @@
+package desafios.basico;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Dragao {
+    public static void main(String[] args) {
+        int casos, poderDeLuta;
+        Locale.setDefault(Locale.US);
+        Scanner ler = new Scanner(System.in);
+
+        casos = ler.nextInt();
+
+        for (int i = 0; i < casos; i++) {
+            poderDeLuta = ler.nextInt();
+            //Implemente a condição adequada para a impressão dos textos conforme o solicitado no desafio:
+            if () {
+                System.out.println();
+            } else {
+                System.out.println();
+            }
+        }
+    }
+}
---
+++ src/desafios/basico/FibonacciFacil.java
@@ -0,0 +1,26 @@
+package desafios.basico;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class FibonacciFacil {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int proximo, anterior = 0, atual = 1;
+        for (int i = 1; i <= N; i++) {
+            //Implemeif (i == N) {
+            //                System.out.println(anterior);
+            //            } else {
+            //                System.out.print(anterior + " ");
+            //                proximo = anterior + atual;
+            //                anterior = atual;
+            //                atual = proximo;
+            //            }nte a condição ideal para que possamos obter os valores solicitados:
+
+
+        }
+    }
+}
+
